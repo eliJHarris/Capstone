58b4be6ca169895350aecd3ed05afe1c48a7e764
arobbi00 <arobbi00@uafs.edu>
2025-10-11 19:58:47 -0400

    api db connection established

--- api/main.py
+++ api/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+from fastapi import FastAPI, HTTPException
+
+DATABASE_URL = 'mysql+pymysql://root:pass@adviseme-db/adviseme'
+engine = create_engine(DATABASE_URL)
+
 
 app = FastAPI()
+@app.get("/db")
+def index():
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT version()"))
+            return{"result": result.scalar()}
+    except Exception as e:
+        return{"result": f"Error connecting to database: {e}"}
 
 @app.get("/")
 async def read_root():
